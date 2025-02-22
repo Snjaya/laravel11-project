9fbd6da19a508970775377e98d25b6b9cadaf41c
Sonjaya <hrmansnjaya@gmail.com>
2025-02-22 20:11:26 +0700

    update blog

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Page</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    <h1>About Page</h1>
-    <img src="img/1121.jpg" alt="" width="200">
-    <section>
-        <h2>Hello, I'm {{ $nama }}</h2>
-        <p>
-            I am a passionate web developer with experience in creating dynamic websites and applications. 
-            I specialize in frontend development, working with HTML, CSS, and JavaScript to build engaging user experiences.
-        </p>
-        <p>
-            My journey into web development started when I was curious about how websites are built and wanted to create my own. 
-            Over the years, I've honed my skills and started working on projects that challenge my creativity and problem-solving abilities.
-        </p>
-        <p>
-            Aside from coding, I enjoy learning new technologies, playing video games, and exploring the latest trends in web development.
-        </p>
-        <p>
-            Feel free to reach out to me via the contact page if you'd like to connect!
-        </p>
-    </section>
-    
-    <footer>
-        <p>&copy; 2025 [Your Name]. All Rights Reserved.</p>
-    </footer>
-    
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3>Ini Adalah Halaman About</h3>
+  </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',['title' => 'Home Page']);
 });
 Route::get('/about', function () {
-    return view('about',['nama' => 'Sonjaya']);
+    return view('about',['nama' => 'Sonjaya','title' => 'About']);
 });
-Route::get('/blog', function () {
-    return view('blog',['nama' => 'Sonjaya']);
+Route::get('/posts', function () {
+    return view('posts',['title' => 'Blog','posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Sonjaya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Ducimus voluptatibus perferendis ullam eveniet 
+            consequuntur aperiam officiis! Nobis cupiditate, 
+            veniam doloremque id delectus doloribus asperiores quisquam obcaecati 
+            cumque, mollitia minima voluptatum.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Herman',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nam quas odit omnis? 
+            Quas omnis libero pariatur dolorem! Architecto tempora voluptate porro ad 
+            numquam repellat pariatur, temporibus quo voluptatum quasi!.'
+        ]
+    ]]);
 });
 Route::get('/contact', function () {
-    return view('contact',['nama' => 'Sonjaya']);
+    return view('contact',['title' => 'Contact']);
 });
