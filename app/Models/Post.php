8a8330a18bedf12cf9ae8e437ad7dd494e2ba409
Sonjaya <hrmansnjaya@gmail.com>
2025-02-28 14:46:22 +0700

    learn and test model factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class Post extends Model
 {
-    protected $fillable = ['title','author','body','slug'];
+    protected $fillable = ['slug','title','author','body'];
+    use \Illuminate\Database\Eloquent\Factories\HasFactory;
 };
 
 
--- database/migrations/2025_02_25_160659_create_posts_table.php
+++ database/migrations/2025_02_25_160659_create_posts_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->unique();
             $table->string('title');
             $table->string('author');
+            $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
         });
